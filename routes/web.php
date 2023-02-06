3157a9add947d3c062baba7e6062d3dd5f954cd7
Your Name <you@example.com>
2023-02-06 15:42:56 +0200

    first commit

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('dashboard.index');
 });
-Route::get('/Kosemak', function () {
-    return view('dashboard.index');
-});
