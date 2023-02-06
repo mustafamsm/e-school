8c72fee5f6d8a6410e9962f3c391f939b8aa5b41
Your Name <you@example.com>
2023-02-06 15:38:43 +0200

    kosison

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('dashboard.index');
 });
+Route::get('/Kosemak', function () {
+    return view('dashboard.index');
+});
